a360f1a7129b8a8e5d1cc9e96ca01e9207fb4459
FelipeAC22 <04471805096@senacrs.edu.br>
2025-09-30 19:17:19 -0300

    adicionada a pasta aula7

---
+++ aula7/script.js
@@ -0,0 +1,24 @@
+// atividade 1 //
+
+let calculo1 = 3 + 4
+let calculo2 = (3 * 5) / 2
+let calculo3 = (4 - 5) * -1
+let calculo4 = 234 % 5
+
+console.log(calculo1, calculo2, calculo3, calculo4)
+
+// atividade 2 //
+
+let primeiroNumero = 5
+let segundoNumero = 10
+
+let igualA = primeiroNumero === segundoNumero
+let diferenteDe = primeiroNumero !== segundoNumero
+let maiorQue = primeiroNumero > segundoNumero
+let menorQue = primeiroNumero < segundoNumero
+
+
+console.log("O primeiro numero é igual ao segundo?", igualA)
+console.log("O primeiro numero é diferente que o segundo?", diferenteDe)
+console.log("O primeiro numero é maior que o segundo?", maiorQue)
+console.log("O primeiro numero é menor que o segundo?", menorQue)
