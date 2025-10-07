c56acc7f76f1d6139ba4ac5e74a1409f109103f4
FelipeAC22 <04471805096@senacrs.edu.br>
2025-10-06 21:44:08 -0300

    adicionado a pasta aula9

---
+++ aula9/exemplos.js
@@ -0,0 +1,30 @@
+//ex1//
+
+const nome = " Felipe Antunes Cardoso "
+
+console.log(nome.length)
+
+let frase = "OlaAaAA"
+let fraseMinuscula = frase.toLowerCase()
+let fraseMaiuscula = frase.toUpperCase()
+
+console.log(fraseMaiuscula, fraseMinuscula)
+
+//ex2//
+
+console.log(nome)
+console.log(nome.trim())
+
+//ex3//
+
+let fraseCenoura = "Hoje comi cenoura, adoro cenoura"
+
+console.log(fraseCenoura.includes("cenoura"))
+console.log(fraseCenoura.includes("batata"))
+
+//ex4//
+
+let novaFrase = fraseCenoura.replaceAll("cenoura", "batata")
+console.log(novaFrase)
+
+
---
+++ aula9/script.js
@@ -0,0 +1,37 @@
+// atividade 1 
+/*
+let nome = prompt("Qual é seu nome?")
+let corFavorita = prompt("Qual é a sua cor favorita?")
+console.log("A cor favorita de " + nome + " é " + corFavorita)
+
+nome = prompt(`Qual é seu nome?`)
+corFavorita = prompt(`Qual é a sua cor favorita?`)
+console.log(`A cor favorita de ${nome} é ${corFavorita}`)
+
+// atividade 2 
+
+let frase = prompt("Escreva uma frase qualquer:")
+
+console.log(`frase com letras maiúsculas: ${frase.toUpperCase()}`)
+console.log(`frase substituindo todas as letras "o" por "i": ${frase.replaceAll("o", "i")}`)
+console.log(`tamanho da frase: ${frase.length}`)
+*/
+// atividade 3 
+
+let raçasDeCachorros = ["Pug", "Shih Tzu", "Pastor Alemão", "Rottweiler", "Caramelo"]
+let numeroEscolhido = Number(prompt("Digite um número entre 0 e 4:"))
+
+console.log(`Raça de cahorro correspondente ao número escolhido: ${raçasDeCachorros[numeroEscolhido]}`)
+
+// atividade 4 
+/*
+let arrayDeNumeros = [1, 2, 3, 4, 5, 6]
+
+console.log(`tamanho do array original: ${arrayDeNumeros.length}`)
+
+arrayDeNumeros.push(7)
+arrayDeNumeros.splice(3, 2)
+
+console.log(`array depois das alterações: ${arrayDeNumeros}`)
+console.log(`novo tamanho do array depois das alterações: ${arrayDeNumeros.length}`)
+*/
